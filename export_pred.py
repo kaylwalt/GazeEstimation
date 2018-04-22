cf4a0bef6ec2c0f44084a94ea9aea9c8b2d805bf
kaylwalt <kaylwalton13@gmail.com>
2018-04-22 19:28:04 -0400

    finally added in a generator to keep the session open for predictions

--- export_pred.py
+++ export_pred.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from medium_face import cnn_model_fn
 
-model_directory = "../model_medium"
 
 def serving_input_receiver_fn():
   """Build the serving inputs."""
@@ -19,4 +18,4 @@ export_dir = classifier.export_savedmodel(
     export_dir_base="../export_model_medium",
     serving_input_receiver_fn=serving_input_receiver_fn)
 
-print(export_dir)
+print("export_dir")
--- test.py
+++ test.py
@@ -2,6 +2,38 @@ import tensorflow as tf
 import numpy as np
 from medium_face import cnn_model_fn
 import time
+
+
+
+class FastPredict:
+    def _createGenerator(self):
+        while not self.closed:
+            yield self.next_features
+
+    def __init__(self, estimator):
+        self.estimator = estimator
+        self.first_run = True
+        self.closed = False
+
+
+    def predict(self, features):
+        self.next_features = features
+        if self.first_run:
+            def data_input_fn():
+                ds = tf.data.Dataset.from_generator(self._createGenerator,
+                                        {"x": tf.float32})
+                return ds
+            self.predictions = self.estimator.predict(data_input_fn)
+            self.first_run = False
+
+        results = []
+        results.append(next(self.predictions))
+        return results
+
+    def close(self):
+        self.closed=True
+        next(self.predictions)
+
 def main(unused_argv):
   # Load training and eval data
 
@@ -15,11 +47,12 @@ def main(unused_argv):
 
   # Create the Estimator
   run_config = tf.estimator.RunConfig().replace(
-    session_config=tf.ConfigProto(device_count={'GPU': 0}))
+    session_config=tf.ConfigProto(device_count={'GPU': 1}))
 
   classifier = tf.estimator.Estimator(
       model_fn=cnn_model_fn, model_dir="../model_medium", config=run_config)
 
+  fp = FastPredict(classifier)
   # Evaluate the model and print results
   # eval_input_fn = tf.estimator.inputs.numpy_input_fn(
   #     x={"x": eval_data},
@@ -31,18 +64,22 @@ def main(unused_argv):
   # print(eval_results)
 
   final = time.clock()
-  for i in range(10):
+  num_entries = 1000
+  for i in range(num_entries):
     start = time.clock()
-    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+    pred_input_fn = tf.estimator.inputs.numpy_input_fn(
         x={"x": eval_data[i:i+1]},
-        y=eval_labels[i:i+1],
         num_epochs=1,
         shuffle=False)
-    pred_results = classifier.predict(input_fn=eval_input_fn, predict_keys=["angles"])
-    print("time taken: ", time.clock() - start)
-    print("eval labels: ", eval_labels[i:i+1])
-    print("predictions: ", list(pred_results))
-  print("total time taken: ", time.clock() - final)
+    #print(tuple(eval_data[i:i+1]))
+    #pred_results = classifier.predict(input_fn=pred_input_fn)
+    pred_results = fp.predict({"x": eval_data[i:i+1]})
+    #print("time taken for {}: ".format(i), time.clock() - start)
+    #print("eval labels: ", eval_labels[i:i+1])
+    #print("predictions: ", list(pred_results))
+  total_time = time.clock() - final
+  print("total time taken: ", total_time)
+  print("predictions per second: ", num_entries / total_time)
 
 if __name__ == "__main__":
     tf.app.run()
